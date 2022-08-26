a2db93d717e8be644ab168b162622fe9b0572d94
NgoGiange <ngogiang190201@gmail.com>
2022-08-26 17:22:46 +0700

    Xây base popup update, usereduce xử lý change data

--- src/components/UsersItem/UsersItem.js
+++ src/components/UsersItem/UsersItem.js
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer, useRef } from 'react';
 
 import styles from './UsersItem.module.scss';
 import Images from '~/components/Images';
@@ -7,12 +7,48 @@ import Buttons from '~/components/Buttons';
 import { HomeIcon, UserGroupIcon } from '~/components/Icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare, faTrashCan } from '@fortawesome/free-regular-svg-icons';
-const cx = classNames.bind(styles);
-function UsersItem({ isAdd = false }) {
-    const [renderUsers, setRenderUsers] = useState([]);
-    const [dataChange, setDataChange] = useState(false);
+import Popup from '~/components/Popup';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
-    const handleDelete = (id) => {
+const cx = classNames.bind(styles);
+
+//Reducer handle change data
+//Init State
+const initState = false;
+
+//Actions
+const ADD_ACTION = 'add';
+const DELETE_ACTION = 'delete';
+
+//Reducer
+const reducer = (state, action) => {
+    console.log('reducer running...');
+    switch (action) {
+        case ADD_ACTION:
+            return !state;
+        case DELETE_ACTION:
+            return !state;
+        default:
+            throw new Error(`Invalid action`);
+    }
+};
+
+function UsersItem({ isAdd = false }) {
+    const popupRef = useRef();
+    const [renderUsers, setRenderUsers] = useState([]);
+    const [dataChange, dispatch] = useReducer(reducer, initState);
+
+    //render list user
+    useEffect(() => {
+        fetch(`http://localhost:3000/users`)
+            .then((res) => res.json())
+            .then((data) => {
+                setRenderUsers(data);
+            });
+    }, [dataChange]);
+
+    //delete user in list user
+    const handleDeleteUser = (id) => {
         const options = {
             method: 'DELETE',
             headers: {
@@ -23,26 +59,39 @@ function UsersItem({ isAdd = false }) {
         fetch(`http://localhost:3000/users/${id}`, options)
             .then((response) => response.json())
             .then(() => {
-                setDataChange(!dataChange);
+                // call dispatch to handle change data when user was deleted
+                dispatch(DELETE_ACTION);
             });
     };
+    //Update user in the list user
+    const handleShowPopupUpdate = (id) => {
+        console.log(popupRef);
+        const popupElement = popupRef.current;
+        const cover = document.querySelector('#cover');
+        const popupClass = cx('popup');
+        const coverClass = cx('cover');
+        popupElement.classList.add(popupClass);
+        cover.classList.add(coverClass);
+    };
+    const handleClosePopupUpdate = () => {
+        const popupElement = popupRef.current;
+        const cover = document.querySelector('#cover');
+        const popupClass = cx('popup');
+        const coverClass = cx('cover');
+        popupElement.classList.remove(popupClass);
+        cover.classList.remove(coverClass);
+    };
 
+    // call dispatch to handle change data when new user was added
     useEffect(() => {
-        fetch(`http://localhost:3000/users`)
-            .then((res) => res.json())
-            .then((data) => {
-                setRenderUsers(data);
-            });
-    }, [dataChange]);
-
-    useEffect(() => {
-        setDataChange(isAdd);
+        dispatch(ADD_ACTION);
     }, [isAdd]);
     return (
         <>
             {renderUsers.map((user) => {
                 return (
                     <div key={user.id} className={cx('user-item')}>
+                        <div id="cover"></div>
                         <p className={cx('position')}>
                             <UserGroupIcon />
                             <strong>{user.department}</strong>
@@ -60,13 +109,65 @@ function UsersItem({ isAdd = false }) {
                             </p>
                         </div>
                         <div className={cx('button')}>
-                            <Buttons outline className={cx('delete-btn')} onClick={() => handleDelete(user.id)}>
+                            <Buttons outline className={cx('delete-btn')} onClick={() => handleDeleteUser(user.id)}>
                                 <FontAwesomeIcon icon={faTrashCan} />
                             </Buttons>
-                            <Buttons outline className={cx('update-btn')}>
+                            <Buttons
+                                outline
+                                className={cx('update-btn')}
+                                onClick={() => handleShowPopupUpdate(user.id)}
+                            >
                                 <FontAwesomeIcon icon={faPenToSquare} />
                             </Buttons>
                         </div>
+                        <Popup ref={popupRef}>
+                            <p className={cx('popup-title')}>
+                                <UserGroupIcon />
+                                Update User
+                            </p>
+                            <FontAwesomeIcon
+                                icon={faXmark}
+                                className={cx('icon-xmark')}
+                                onClick={handleClosePopupUpdate}
+                            />
+                            <div className={cx('update-popup')}>
+                                <div className={cx('avatar-user')}>
+                                    <div className={cx('update-avatar')}>
+                                        <img className={cx('update-image')} src="" alt="" id="avatar" />
+                                    </div>
+                                    <input
+                                        type="file"
+                                        className={cx('upload-avatars')}
+                                        accept="image/png , image/jpeg"
+                                    />
+                                </div>
+                                <div className={cx('upload-infor')}>
+                                    <label htmlFor="name">Name User</label>
+                                    <input type="text" className={cx('upload-name')} id="name" spellCheck={false} />
+                                    <label htmlFor="age">Age User</label>
+                                    <input type="text" className={cx('upload-age')} id="age" spellCheck={false} />
+                                    <label htmlFor="address">Address User</label>
+                                    <input
+                                        type="text"
+                                        className={cx('upload-address')}
+                                        id="address"
+                                        spellCheck={false}
+                                    />
+                                    <label htmlFor="position">Position User</label>
+                                    <input
+                                        type="text"
+                                        className={cx('upload-position')}
+                                        id="position"
+                                        spellCheck={false}
+                                    />
+                                    <label htmlFor="phone">Number Phone User</label>
+                                    <input type="text" className={cx('upload-phone')} id="phone" spellCheck={false} />
+                                </div>
+                            </div>
+                            <Buttons primary className={cx('confirm-btn')}>
+                                <FontAwesomeIcon icon={faPenToSquare} className={cx('icon-update')} />
+                            </Buttons>
+                        </Popup>
                     </div>
                 );
             })}
